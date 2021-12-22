139a03fcfcaab1030f359110dda4dc166c7e62fd
panda1114Git <panda.kk1114@gmail.com>
2021-12-22 12:33:13 +0000

    background color

--- src/components/CopTicket.js
+++ src/components/CopTicket.js
@@ -9,8 +9,20 @@ const Endsin = () => (
         <p>Ends in:    5d   10h  30m  13s</p>
     </div>
 );
+
 export const CopTicket = () => (
-    <div>
+    <div className='cop-ticket'>
         <Endsin></Endsin>
+        <div className='win-bored'>
+            <p>Win Bored ape #4252</p>
+        </div>
+        <div className='content'>
+            <p>We want everyone to have equal opportunity, 
+            especially those whod normally wouldnt have a change or voice 
+            in this space to own a iconic NFT</p>
+        </div>
+        <div className='avatar'>
+            <img src='../images/cop_avatar.png'></img>
+        </div>
     </div>
 );
