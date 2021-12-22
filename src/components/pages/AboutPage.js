f04e700b963fdbe455d3b992647c8e4ac1ab9373
panda1114Git <panda.kk1114@gmail.com>
2021-12-22 10:39:48 +0000

    button initiated

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -4,10 +4,14 @@ import React from 'react';
 import CustomButton from '../common/CustomButton'
 // import { HoverButtonDiagonal } from '../common/button';
 // COMPONENT
-
+const style = {
+    color: 'red',
+    fontSize: 200
+  };
 const AboutPage = () => (
+    
     <main className="p-3 animated fadeIn">
-        <CustomButton className="button" onClick={ () => alert('adf') }>first</CustomButton>
+        <CustomButton className="button" style ={style} > erdsf </CustomButton>
     </main>
 );
 
