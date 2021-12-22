39f6322226c243f607b83c109c8d0053609686b6
panda1114Git <panda.kk1114@gmail.com>
2021-12-22 14:14:05 +0000

    horizon font

--- src/components/CopTicket.js
+++ src/components/CopTicket.js
@@ -1,7 +1,7 @@
 // IMPORT PACKAGE REFERENCES
 
 import React from 'react';
-
+import CopAvatar from '../images/cop_avatar.png';
 // COMPONENT
 
 const Endsin = () => (
@@ -10,19 +10,50 @@ const Endsin = () => (
     </div>
 );
 
+
 export const CopTicket = () => (
-    <div className='cop-ticket'>
-        <Endsin></Endsin>
-        <div className='win-bored'>
-            <p>Win Bored ape #4252</p>
+    <div>
+        <div className='ticket-board'>
+            <Endsin />
+            <div className='win-bored'>
+                <p>Win Bored ape #4252</p>
+            </div>
+            <div className='content'>
+                <p>We want everyone to have equal opportunity, 
+                especially those whod normally wouldnt have a change or voice 
+                in this space to own a iconic NFT</p>
+            </div>
+            <div className='cop-ticket-group'>
+                <div className='cop-ticket-button-group'>
+                    <input type={'number'}></input>
+                    <button>Cop a ticket</button>
+                </div>
+                <div className='price'>
+
+                    <div className='name'>Price</div>
+
+                    <div className='value'> 0.0062
+                    <i className="fab fa-bitcoin"></i>
+                    </div>
+
+                </div>
+                <div className='odds'>
+                    <div className='name'>Odds</div>
+
+                    <div className='value'> 0.01%</div>
+                
+
+                </div>
+            </div>
         </div>
-        <div className='content'>
-            <p>We want everyone to have equal opportunity, 
-            especially those whod normally wouldnt have a change or voice 
-            in this space to own a iconic NFT</p>
+        <div className='ticket-image'>
+            <img src={ CopAvatar} alt ="avatar"></img>
         </div>
+        
+        
+        
         <div className='avatar'>
-            <img src='../images/cop_avatar.png'></img>
+            
         </div>
     </div>
 );
