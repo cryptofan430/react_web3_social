c50f83f24c0d363796046dc5513178cdf5ec765f
assasin124 <liming19940725@gmail.com>
2021-12-23 20:50:36 +0200

    button style issue

--- src/components/common/button/index.js
+++ src/components/common/button/index.js
@@ -5,39 +5,26 @@ import './style.scss';
 // React Component
 function Button(props) {
     const {
-        button_type,
-        width,
-        height,
-        fontSize,
+        button_type,       
         children,
         onClick} = props;
     return <button 
             onClick={onClick}
             className={button_type }
-            style={{
-                height,
-                width,
-                fontSize,
-            }}>
+           >
             {children}
         </button>
 }
 
 Button.propTypes = {
     button_type: PropTypes.string,
-    children: PropTypes.string,
-    height: PropTypes.string,
-    width: PropTypes.string,
-    onClick: PropTypes.func,
-    fontSize: PropTypes.string
+    children: PropTypes.string,   
+    onClick: PropTypes.func,    
 };
 
-Button.defaultProps = {
-    width: "259px",
-    height: "72px",
+Button.defaultProps = {  
     children: "Connect",
-    button_type: "tk_outlind",
-    fontSize: "17px"
+    button_type: "tk_outlind",   
 }
 
 export default Button;
