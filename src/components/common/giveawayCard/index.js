ed2c17a2a2ce784cb1aa79b715060bc5dee8f559
assasin124 <liming19940725@gmail.com>
2021-12-24 00:01:33 +0200

    updated result

---
+++ src/components/common/giveawayCard/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+function GiveawayCard(props) {   
+    return <div className="giveaway-component">
+            <div className="details">
+                <div className="title">{ props.title }</div>
+                <div className="tikkets">
+                    <p className="name">Tikkets</p>
+                    <p className="value">{ props.tikkets_value }</p>
+                </div>
+                <div className="odds">
+                    <p className="name">Odds</p>
+                    <p className="value">{ props.odds_value }%</p>
+                </div>
+                <div className="total-price">
+                    <p className="name">Total price</p>
+                    <p className="value"><span>$</span>{ props.total_price_value }</p>
+                </div>
+            </div>
+            <div className="image">
+                <img src={ props.image } alt="giveawayimage" />
+            </div>
+        </div>   
+}
+
+GiveawayCard.propTypes = {
+    title: PropTypes.string,
+    tikkets_value: PropTypes.string,
+    odds_value: PropTypes.string,
+    total_price_value: PropTypes.string,
+    image: PropTypes.node
+}
+
+export default GiveawayCard;
--- src/components/pages/giveaway/index.js
+++ src/components/pages/giveaway/index.js
@@ -1,37 +1,33 @@
 // IMPORT PACKAGE REFERENCES
 import React from 'react';
-import '../../../styles/components/giveaway.scss'
-// IMPORT PROJECT REFERENCES
+import '../../../styles/components/giveaway.scss';
+import GiveawayCard from '../../common/giveawayCard';
 
-// COMPONENT
+import GiveawayImage from '../../../images/giveaway_image1.png';
 
 export const After = () => (
-    <div className="after"> 
+    <div className="after">
         <div className='notify'>
-            <div className='horizon giveaway'>
-                <div>
-                    <span style={{fontFamily: 'Horizon'}}> YOU JOINED</span>
-                </div>
-                <div>
-                    <span>0 </span><span style={{fontFamily: 'Horizon'}}>GIVEAWAYS</span>
-                </div>
+            <div className='joined-giveaway'>
+                <p>YOU JOINED</p>
+                <p><span>0</span>GIVEAWAYS</p>
             </div>
             <div className='earned'>
-                <div className='text'>
-                    With a total earned value in USD
-                </div>
-                <div className='horizon amount'>
-                    $ 0
-                </div>
+                <p className='text'>With a total earned value in USD</p>
+                <p className='amount'><span>$</span>0</p>
             </div>
         </div>
         <div className='giveaway-list'>
             <div className='active'>
-                <div className='title'>
-                    Active giveaways
-                </div>
+                <p className='title'>Active giveaways</p>
                 <div className='list'>
-
+                    <GiveawayCard 
+                        title="BAYC giveaway #001" 
+                        tikkets_value="242" 
+                        odds_value="8" 
+                        total_price_value="7260" 
+                        image={GiveawayImage} 
+                    />
                 </div>
             </div>
             <div className='previous'>
@@ -43,6 +39,5 @@ export const After = () => (
                 </div>
             </div>
         </div>
-       
     </div>
 );
