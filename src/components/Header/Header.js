ca586dedff299cc5277db4125d4b7a269f194b42
assasin124 <liming19940725@gmail.com>
2021-12-24 10:14:11 +0200

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,42 @@
 // IMPORT PACKAGE REFERENCES
 
 import React from 'react';
-import Button from '../common/button';
+import { NavLink } from 'react-router-dom';
+import Button from '../../components/common/button';
+
 // COMPONENT
 
 export const Header = () => (
-   <header>
-       <div className='header-nav'>
-           <div className='logo'>TIKKETS</div>
-            <div className='latest'>Latest drop</div>
-            <div className='previous'>Previous drop</div> 
-        </div>
+    <nav className="navbar navbar-expand-lg ">
+        <a className="navbar-brand" href="/">
+            <div className='logo'>TIKKETS</div>
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
+            <svg width="27" height="18" viewBox="0 0 27 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 9L27 9" stroke="white" strokeWidth="0.5"/>
+                <path d="M0 1L27 0.999998" stroke="white" strokeWidth="0.5"/>
+                <path d="M0 17L27 17" stroke="white" strokeWidth="0.5"/>
+            </svg>
+        </button>
 
-       <div className='hover-button'>
-            <Button button_type="tk_outlined" onClick = {() => {}}>Connect</Button>
-       </div>
-   </header>
+        <div className="collapse navbar-collapse" id="menu">
+            <ul className="navbar-nav ">
+                <li className="nav-item">
+                    <div className="nav-link">
+                        <NavLink to='/' activeClassName='menu selected' className="latest-drop" exact={true}>Latest drop</NavLink>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <div className="nav-link">
+                        <NavLink to='/zipcodes' activeClassName='menu selected' className="previous-drop">Previous drop</NavLink>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <div className="nav-link">
+                        <Button button_type="tk_outlined" >But a Tikket</Button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
 );
